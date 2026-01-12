9ba14bf5f1c8429d5d4e4ba37ab3fb06a7b25997
JediYoshi <jediyoshi@proton.me>
2026-01-12 10:21:21 -1000

    WIP Main Page (Pre-Vulnerability Patch)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,26 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-				<h1 className="font-extrabold text-5xl text-white tracking-tight sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">First Steps →</h3>
-						<div className="text-lg">
-							Just the basics - Everything you need to know to set up your
-							database and authentication.
-						</div>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank"
-					>
-						<h3 className="font-bold text-2xl">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to
-							deploy it.
-						</div>
-					</Link>
-				</div>
+		<main className="flex list-item px-[25%] pt-[12px]">
+			<div className="w-full min-w-full card bg-base-300 border border-[#747474] shadow-xl my-[8px] p-[8px] gap-[8px]">
+				<div className="text-[24px]"><a className="italic font-bold">808penguinlover</a> thought...</div>
+				<div className="text-[16px] italic">What if I just be Grian and press this button?</div>
+				<div className="text-[10px] italic text-[#959595]">Some time ago...</div>
+			</div>
+			<div className="w-full min-w-full card bg-base-300 border border-[#747474] shadow-xl my-[8px] p-[8px] gap-[8px]">
+				<div className="text-[24px]"><a className="italic font-bold">anon</a> wondered...</div>
+				<div className="text-[16px] italic">Will Minecraft 1.22 happen?</div>
+				<div className="text-[10px] italic text-[#959595]">Some time ago...</div>
+			</div>
+			<div className="w-full min-w-full card bg-base-300 border border-[#747474] shadow-xl my-[8px] p-[8px] gap-[8px]">
+				<div className="text-[24px]"><a className="italic font-bold">the.soulblade.was.taken</a>'s brain said...</div>
+				<div className="text-[16px] italic">Minecraft Bedrock is just gone at this point.</div>
+				<div className="text-[10px] italic text-[#959595]">Some time ago...</div>
+			</div>
+			<div className="w-full min-w-full card bg-base-300 border border-[#747474] shadow-xl my-[8px] p-[8px] gap-[8px] h-[10000px]">
+				<div className="text-[24px]"><a className="italic font-bold">e</a>'s brain said...</div>
+				<div className="text-[16px] italic">e</div>
+				<div className="text-[10px] italic text-[#959595]">Some time ago...</div>
 			</div>
 		</main>
 	);
